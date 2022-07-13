7fdafa6eb9b85490a149a444c82e4d464bf46fa7
hee <dreamq1122@gmail.com>
2022-07-13 12:05:39 +0900

    first commit

---
+++ iMember.java
@@ -0,0 +1,8 @@
+package com.human.member;
+
+import java.util.ArrayList;
+
+public interface iMember {
+	ArrayList<memberDTO> listMember();
+	void insert(String id,String pwd,String name,String mobile);
+}
